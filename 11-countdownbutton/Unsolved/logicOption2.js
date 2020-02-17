a1739f662abdfd658972afdbd75bd224e017ab8a
Casey Carroll <caseydean8@gmail.com>
2020-02-17 09:54:53 -0500

    Buttons update database

--- 11-countdownbutton/Unsolved/logicOption2.js
+++ 11-countdownbutton/Unsolved/logicOption2.js
@@ -10,8 +10,8 @@ const firebaseConfig = {
   measurementId: "G-V3HQ3TT49W"
 };
 console.log("here");
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //  firebase.analytics();
 var database = firebase.database();
 // console.log(database);
@@ -22,30 +22,37 @@ var clickCounter = initialValue;
 
 // --------------------------------------------------------------
 // At the initial load, get a snapshot of the current data.
-database.ref().on("value", function(snapshot) {
-  console.log(snapshot.val());
-})
+database.ref().on(
+  "value",
+  function(snapshot) {
+    // Print the initial data to the console.
+    console.log(snapshot.val());
+    // Change the clickCounter to match the data in the database
+    clickCounter = snapshot.val().clicks;
+    // Change the html to reflect the initial value.
+    $("#click-value").text(clickCounter);
+  },
+  function(errorObj) {
+    console.log(`read failed: ${errorObj.code}`);
+  }
+);
 
 $("#click-button").on("click", function() {
   clickCounter--;
   database.ref().set({
     clicks: clickCounter
   });
-})
-// Print the initial data to the console.
+});
 
-
-// Change the html to reflect the initial value.
-
-
-// Change the clickCounter to match the data in the database
-
+$("#restart-button").on("click", function() {
+  database.ref().set({
+    clicks: initialValue
+  });
+});
 
 // Log the value of the clickCounter
 
-
 // Change the HTML Value
-
 
 // If any errors are experienced, log them to console.
 
@@ -53,33 +60,16 @@ $("#click-button").on("click", function() {
 
 // Whenever a user clicks the click button
 $("#click-button").on("click", function() {
-
   // Reduce the clickCounter by 1
-
-
   // Alert User and reset the counter
-
-
   // Save new value to Firebase
-
-
   // Log the value of clickCounter
-
-
 });
 
 // Whenever a user clicks the restart button
 $("#restart-button").on("click", function() {
-
   // Set the clickCounter back to initialValue
-
-
   // Save new value to Firebase
-
-
   // Log the value of clickCounter
-
-
   // Change the HTML Values
-
 });
